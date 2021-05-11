5b3938fe4737ef54b1548f48b04d7a3c16449421
ardiansyah-sweng <emailyas2013@gmail.com>
2021-05-11 10:34:47 +0700

    update

--- pso/psorigin.php
+++ pso/psorigin.php
@@ -6,13 +6,6 @@ class MPUCWPSO
 {
     private $PRODUCTIVITY_FACTOR = 20;
     private $FITNESS_VALUE_BASELINE = array(
-        'azzeh1' => 1219.8,
-        'azzeh2' => 201.1,
-        'azzeh3' => 1564.8,
-        'silhavy' => 240.19,
-        'karner' => 1820,
-        'nassif' => 1712,
-        'ardiansyah' => 404.85,
         'polynomial' => 238.11
     );
 
@@ -51,36 +44,6 @@ class MPUCWPSO
         return $arrPartikel[array_search(min($ae), $ae)];
     }
 
-    function uniformInitialization()
-    {
-        $n = $this->swarm_size;
-        $X1 = $this->randomUCWeight();
-        for ($i = 1; $i <= $n - 1; $i++) {
-            $R[$i] = $this->randomUCWeight();
-        }
-        foreach ($R as $key => $r) {
-            $xSimple = $X1['xSimple'] + $r['xSimple'] / $n * ($this->range_positions['max_xSimple'] - $this->range_positions['min_xSimple']);
-            $xAverage = $X1['xAverage'] + $r['xAverage'] / $n * ($this->range_positions['max_xAverage'] - $this->range_positions['min_xAverage']);
-            $xComplex = $X1['xComplex'] + $r['xComplex'] / $n * ($this->range_positions['max_xComplex'] - $this->range_positions['min_xComplex']);
-
-            if ($xSimple > $this->range_positions['max_xSimple']) {
-                $xSimple = $xSimple - ($this->range_positions['max_xSimple'] - $this->range_positions['min_xSimple']);
-            }
-            if ($xAverage > $this->range_positions['max_xAverage']) {
-                $xAverage = $xAverage - ($this->range_positions['max_xAverage'] - $this->range_positions['min_xAverage']);
-            }
-            if ($xComplex > $this->range_positions['max_xComplex']) {
-                $xComplex = $xComplex - ($this->range_positions['max_xComplex'] - $this->range_positions['min_xComplex']);
-            }
-
-            if (($key - 1) == 0) {
-                $ret[0] = $X1;
-            }
-            $ret[$key] = ['xSimple' => $xSimple, 'xAverage' => $xAverage, 'xComplex' => $xComplex];
-        }
-        return $ret;
-    }
-
     function randomUCWeight()
     {
         $ret['xSimple'] = mt_rand($this->range_positions['min_xSimple'] * 100, $this->range_positions['max_xSimple'] * 100) / 100;
@@ -105,20 +68,6 @@ class MPUCWPSO
         return ($parameters['w'] * $parameters['velocity']) + (($parameters['c1'] * $parameters['r1']) * ($parameters['pbest'] - $parameters['position'])) + (($parameters['c2'] * $parameters['r2']) * ($parameters['gbest'] - $parameters['position']));
     }
 
-    function RIW($particle, $particles)
-    {
-        array_multisort(array_column($particles, 'ae'), SORT_ASC, $particles);
-        $rank = array_search($particle['ae'], array_column($particles, 'ae'));
-        $b = 1;
-        if ($rank <= ($this->swarm_size/4) ){
-            $b = 2/3;
-        }
-        if ($rank >= (3 * $this->swarm_size)/4 ){
-            $b = 1.5;
-        }
-        return $b;
-    }
-
     function Main($dataset, $max_iter, $swarm_size, $max_counter, $chaotic_type)
     {
         //check if there are particles exceeds the lower or upper limit
@@ -130,20 +79,18 @@ class MPUCWPSO
 
         ##Masuk Iterasi
         for ($iterasi = 0; $iterasi <= $max_iter; $iterasi++) {
-            $chaoticFactory = new ChaoticFactory();
-            $chaos = $chaoticFactory->initializeChaotic($chaotic_type, $iterasi);
             $r1 = $this->randomZeroToOne();
             $r2 = $this->randomZeroToOne();
+            $w = $this->INERTIA_MIN - ((($this->INERTIA_MAX - $this->INERTIA_MIN) * $iterasi) / $max_iter);
 
             ##Generate Population
             if ($iterasi === 0) {
-                $I[$iterasi + 1] = $iterasi;
                 $vSimple[$iterasi + 1] = $this->randomZeroToOne();
                 $vAverage[$iterasi + 1] = $this->randomZeroToOne();
                 $vComplex[$iterasi + 1] = $this->randomZeroToOne();
 
                 for ($i = 0; $i <= $swarm_size - 1; $i++) {
-                    $positions = $this->uniformInitialization()[$i];
+                    $positions = $this->randomUCWeight();
                     $UCP = $this->size($positions, $dataset);
                     $estimated_effort = $UCP * $this->PRODUCTIVITY_FACTOR;
                     $particles[$iterasi + 1][$i] = [
@@ -161,29 +108,7 @@ class MPUCWPSO
             if ($iterasi > 0) {
                 //Update Velocity dan X_Posisi
                 for ($i = 0; $i <= $swarm_size - 1; $i++) {
-                   
-                    //Inertia weight
-                    $w_ini = $this->INERTIA_MAX;
-                    $w_fin = $this->INERTIA_MIN;
-
-                    $chaos->I = $I[$iterasi];
-                    $cosine = $chaos->chaotic($max_iter);
-                    $w_cos = ((($w_ini + $w_fin) / 2) + (($w_ini - $w_fin) / 2)) * $cosine;
-
-                    $b = $this->RIW($particles[$iterasi][$i], $particles[$iterasi]);
-                    $w = $b * $w_cos;
-                    
-                    if (($I[$iterasi] <= $max_iter) / 6) {
-                        $a = 4 / 3;
-                    }
-                    if (($max_iter / 6) < $I[$iterasi] && $I[$iterasi] <= (5 * $max_iter) / 6) {
-                        $a = 16 / 3;
-                    }
-                    if ((5 * $max_iter) / 6 < $I[$iterasi] && $I[$iterasi] <= $max_iter) {
-                        $a = 2 / 9;
-                    }
-                    $I[$iterasi + 1] = $I[$iterasi] + $a;
-
+                
                     $vSimples = [
                         'w' => $w,
                         'velocity' => $vSimple[$iterasi],
@@ -396,7 +321,7 @@ function get_combinations($arrays)
 $combinations = get_combinations(
     array(
         //        'particle_size' => array(10),
-        'chaotic' => array('cosine'),
+        //'chaotic' => array('cosine'),
         'particle_size' => array(10, 20, 30, 40, 50, 60, 70, 80, 90, 100),
         // 'chaotic' => array('bernoulli', 'chebyshev', 'circle', 'gauss', 'logistic', 'sine', 'singer', 'sinu'),
     )
@@ -421,8 +346,8 @@ foreach ($combinations as $key => $combination) {
     print_r($combination);
     echo '<br>';
 
-    $data = array($mae, $combination['particle_size'], $combination['chaotic']);
-    $fp = fopen('results/zhang2021.txt', 'a');
+    $data = array($mae, $combination['particle_size']);
+    $fp = fopen('results/psorigin.txt', 'a');
     fputcsv($fp, $data);
     fclose($fp);
 }
